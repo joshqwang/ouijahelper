fef1125c38c30b5d589637596b982b7d137a729b
Sross314 <80728221+Sross314@users.noreply.github.com>
2021-03-16 11:35:35 -0500

    Update bot.py

--- ouijahelper/bot.py
+++ ouijahelper/bot.py
@@ -13,19 +13,19 @@ async def on_ready():
 
 
 @client.event
-async def hello_world(message):
+async def on_message(message):
     if message.author == client.user:
         return
     if message.content.lower() == 'hello':
         await message.channel.send('World!')
         
 #Prints out information about the bot and how to use its functions
-#*@client.event
-#async def ouija_help(message):
-#    if message.author == client.user:
-#        return
-#    if message.content.lower() == '.help':
-#        await message.channel.send('Info about commands and stuff')
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.lower() == '.help':
+        await message.channel.send('Info about commands and stuff')
 
 
 
